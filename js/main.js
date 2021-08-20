b27fef39a1764abf53ce0ca9dfadf0600f6becbc
apkoha <79258179+apkoha@users.noreply.github.com>
2021-08-20 19:38:58 +0300

    Add files via upload
    Дописываем модалку и JS

--- js/main.js
+++ js/main.js
@@ -3,6 +3,34 @@ $(document).ready(function () {
   var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
+  var flatsPath = $(".modal-image path");
+  var flatsLink = $(".flat-link");
+
+  flatsPath.on("mouseover", function() { // при наведении курсора на квартиру на плане
+    deleteClass();
+    $(`[data-flat-link="${$(this).attr("data-flat")}"]`).toggleClass("current-flat");
+  });
+
+  flatsLink.on("mouseover", function() { // при навдение курсора на номер квартиры
+    deleteClass();
+    $(`[data-flat="${$(this).attr("data-flat-link")}"]`).toggleClass("current-flat");
+  });
+
+  function deleteClass() {
+    flatsPath.removeClass("current-flat");
+    flatsLink.removeClass("current-flat");
+  }
+
+  flatsLink.on("mouseout", function() {
+    flatsPath.removeClass("current-flat")
+  });
+
+  flatsPath.on("mouseout", function() {
+    flatsLink.removeClass("current-flat")
+  });
 
   // функция при наведении мышкой на этаж
   floorPath.on("mouseover", function() {
@@ -10,6 +38,10 @@ $(document).ready(function () {
     currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
     $(".counter").text(currentFloor) // записываем значение этажа в счётчик справа
   });
+
+  floorPath.on("click", toggleModal); // клик по этажу на картинке с домом
+  modalCloseButton.on("click", toggleModal); // клик по кнопке закрытия модалки
+  viewFlatsButton.on("click", toggleModal);
 
   counterUp.on("click", function () { // клик по кнопке вверх
     if (currentFloor < 18) { // задаём условие счётчика максимального этажа
@@ -21,13 +53,16 @@ $(document).ready(function () {
     }
   });
 
-  counterDown.on("click", function () {
+  counterDown.on("click", function () { // клик по кнопке вниз
     if (currentFloor > 2) {
-      currentFloor--;
-      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false});
+      currentFloor--; // отнимаем один этаж
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false}); // добавляем к цифре этажей меньше 10 "0"
       $(".counter").text(usCurrentFloor); // записываем значение этажа в счётчик справа
       floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
     }
   });
+  function toggleModal() { // функция закрыть окно
+    modal.toggleClass("is-open");
+  }
 });
